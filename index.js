e269d7b58bc2bf9a1b5821f49ff119ee9ef0430d
Karim <karimkhalifa110@gmail.com>
2021-12-31 21:43:18 +0200

    actual code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORt ||80;
+var port = process.env.PORT ||3000;
 var currRes;
 
 //index route
