df39fbadf83f5d6baf5bf649af8b88f6b3b5ccc3
Karim <karimkhalifa110@gmail.com>
2021-12-31 21:27:12 +0200

    port fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORt ||3000;
+var port = process.env.PORt ||80;
 var currRes;
 
 //index route
